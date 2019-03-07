e504ede7f3cb66dfc77e33bb4bf31dee3b51b17a
reneew <reneew@outlook.com>
2019-03-07 12:02:49 -0800

    Find repetition values

--- sim_function.R
+++ sim_function.R
@@ -18,5 +18,26 @@ find_sim <- function(artist_name, song_name) {
   bestsongsim
 }
 
+find_sim_val <- function(artist_name, song_name) {
+  find_sim(artist_name, song_name)$repetitiveness * 100 
+}
+
+# Code for calculating repetition values for each song, keep commented out
+# us_top <- read.csv("data/us_top200.csv", stringsAsFactors = FALSE)
+# itrs <- nrow(us_top)
+# ls <- vector("list", length = itrs)
+# for (i in 1:itrs) {
+#   curr <- us_top[i,]
+#   curr_artist <- curr$Artist
+#   curr_song <- curr$Track.Name
+#   ls [[i]] <- find_sim_val(curr_artist, curr_song)
+# }
+# ls[1:200]
+# v <- unlist(ls, use.names = FALSE)
+# df <- data.frame(v)
+#write.csv("data/calculated_sim.csv")
+
+vals <- read.csv("data/calculated_sim.csv", stringsAsFactors = FALSE)
+
 # Test Code
-find_sim("Taylor Swift", "Enchanted")
+find_sim_val("Bruno Mars", "Thats What I Like")
